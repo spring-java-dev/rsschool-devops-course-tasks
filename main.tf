7237124abc1b7d29a4626b7fb197cdf512a5749a
SpringJavaDev <spring-java-dev@mail.ru>
2025-06-09 19:16:30 +0400

    feat: minor

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
@@ -18,11 +22,11 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
           StringEquals = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+            StringLike = {
+              "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+            }
           }
         }
-      }
       }
     ]
   })
